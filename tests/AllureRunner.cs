a49d102ca2b39f2eb16a4d75b2f60e09c321aa9b
unknown <moore.evelyn13@gmail.com>
2021-07-23 19:10:03 +0300

    tryind to execute shell command from code

--- tests/AllureRunner.cs
+++ tests/AllureRunner.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -12,6 +13,36 @@ namespace WebTests
     public class AllureRunner
     {
         public const string TestPlanEnv = "ALLURE_TESTPLAN_PATH";
+
+        public static TestPlan? getTestPlan()
+        {
+            var testPlanPath = getTestPlanPath();
+            // if (!(testPlanPath != null && File.Exists(testPlanPath)))
+            // {
+            //     return null;
+            // }
+            
+            try
+            {
+                var testPlanJson = @"{'version':'1.0','tests':[{'id':'1','selector':'WebTests.DuckduckgoTest.DuckduckgoSearchTest'},{'id':'2','selector':'WebTests.EcosiaTest.EcosiaSearchTest'}]}";
+                
+                var options = new JsonSerializerOptions();
+                options.PropertyNameCaseInsensitive = true;
+                var plan = JsonSerializer.Deserialize<TestPlan>(testPlanJson.Replace("'", "\""), options);
+                return plan;
+            }
+
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        public static string? getTestPlanPath()
+        {
+            var EnvPath = Environment.GetEnvironmentVariable(TestPlanEnv);
+            return EnvPath;
+        }
 
         public static void Main(string[] args)
         {
@@ -28,50 +59,31 @@ namespace WebTests
                 var testPlan = getTestPlan();
                 if (testPlan != null)
                 {
+                    List<string> testCaseNames = new List<string>();
                     foreach (var testCase in testPlan.Tests)
                     {
-                        builder.AddTest(testCase.Selector);
+                        testCaseNames.Add(testCase.Selector);
+                        //builder.AddTest(testCase.Selector);
                     }
+                    string testFilter = String.Join('|', testCaseNames);
+                    string strCmdText= "dotnet test --filter \"" + testFilter + "\"";
+                    var startInfo = new ProcessStartInfo()
+                    {
+                        FileName = "powershell.exe",
+                        Arguments = $"-Command {strCmdText}",
+                        UseShellExecute = false
+                    };
+                    Process.Start(startInfo);
                 }
 
-                var filter = builder.GetFilter();
-                using (ITestRunner runner = engine.GetRunner(package))
-                {
-                    var result = runner.Run(listener: null, filter: filter);
-                }
+                // var filter = builder.GetFilter();
+                // using (ITestRunner runner = engine.GetRunner(package))
+                // {
+                //     var result = runner.Run(listener: null, filter: filter);
+                    
+                // }
             }
         }
-        
-        public static TestPlan? getTestPlan()
-        {
-            var testPlanPath = getTestPlanPath();
-            if (!(testPlanPath != null && File.Exists(testPlanPath)))
-            {
-                return null;
-            }
-            
-            try
-            {
-                var testPlanJson = File.ReadAllText(testPlanPath);
-                
-                var options = new JsonSerializerOptions();
-                options.PropertyNameCaseInsensitive = true;
-                var plan = JsonSerializer.Deserialize<TestPlan>(testPlanJson.Replace("'", "\""), options);
-                return plan;
-            }
-
-            catch (Exception e)
-            {
-                return null;
-            }
-        }
-        
-        public static string? getTestPlanPath()
-        {
-            var EnvPath = Environment.GetEnvironmentVariable(TestPlanEnv);
-            return EnvPath;
-        }
-
     }
 
     public class TestPlan
