7bbd9c088ae7258c54bd0c1910ea4c8aeb3e994a
rina-tenitska <moore.evelyn13@gmail.com>
2025-01-28 10:06:58 +0200

    set --user-data-dir to current user dir

--- BrowserTest.cs
+++ BrowserTest.cs
@@ -62,7 +62,7 @@ namespace Web.Tests
                 {
                     ChromeOptions options = new ChromeOptions();
                     Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
-                    options.AddArgument($"--user-data-dir={Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}");
+                    options.AddArgument($"--user-data-dir={Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}1");
                     if (Settings.WebDriver.Headless) 
                     {
                         options.AddArgument("--headless");
